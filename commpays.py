27c342b273a9b06b1ffc09f27f88c19278676c3a
siroghik <siroghik@gmail.com>
2016-03-26 22:03:06 +0200

    add main menu add change tariff add show tariff

--- commpays.py
+++ commpays.py
@@ -5,12 +5,24 @@ water = 'water'
 el_ty = 'el_ty'
 
 def enter_readings(r):
+	if r == 'el_tariff':
+		under_100 = enter_readings('el-ty under 100 kWh')
+		over_100 = enter_readings('el-ty over 100 kWh')
+		return [under_100, over_100]
 	while True:
 		try:
 			v = float(input(r + ': '))
 			return v
 		except:
 			print 'invalid format!'
+
+def print_tariff():
+	print '-' * 8
+	print '{0}: {1} for {2}'.format(gas, tariff[gas], 'm3')
+	print '{0}: {1} for {2}'.format(water, tariff[water], 'm3')
+	print '{0} under 100 kWh: {1} for {2}'.format(el_ty, tariff[el_ty][0], 'kWh')
+	print '{0} over 100 kWh: {1} for {2}'.format(el_ty, tariff[el_ty][1], 'kWh')
+	print '-' * 8 + '\n'
 
 def print_readings(r):
 	print '-' * 8
@@ -50,6 +62,10 @@ print '\n' + '-' * 8
 print 'commpays'
 print '-' * 8
 
+# load tariff
+with open('tariff.json', 'r') as f:
+	tariff = json.load(f)
+
 # load old records
 with open('readings.json', 'r') as f:
 	readings = json.load(f)
@@ -57,47 +73,68 @@ with open('readings.json', 'r') as f:
 # count old records
 quantity = len(readings)
 if(quantity != 0):
-	print '\nYou have ' + str(quantity) + ' records\n'
+	print '\nYou have ' + str(quantity) + ' records'
 	prev_readings = readings[quantity-1]
 	print 'last record:'
 	print_readings(prev_readings)
 else:
-	print '\nyou have no records\n'
+	print '\nyou have no records'
 	prev_readings ={
 		gas: 0.0,
 		water: 0.0,
 		el_ty: 0.0
 	}
 
-# enter new readings
-print 'please enter current readings'
-curr_readings = {
-	gas: enter_readings(gas),
-	water: enter_readings(water),
-	el_ty: enter_readings(el_ty)
-}
-print '\n'
-
-# calculate amount
-amount = {
-	gas: curr_readings[gas] - prev_readings[gas],
-	water: curr_readings[water] - prev_readings[water],
-	el_ty: curr_readings[el_ty] - prev_readings[el_ty]
-}
-
-# load tariff
-with open('tariff.json', 'r') as f:
-	tariff = json.load(f)
-
-# print info
-print_amount()
-calculate()
-
-# save current readings?
-if 'y' == raw_input('wanna save current record? "y/n" '):
-	readings.append(curr_readings)
-	with open('readings.json', 'w') as f:
-		json.dump(readings, f)
+# main menu
+while True:
+	menu_text = """
+what do you want to do?
+n - new readings
+t - change tariff
+s - show tariff
+q - quit
+"""
+	choise = raw_input(menu_text)
+	# creat new readings
+	if choise == 'n':
+		print 'please enter current readings'
+		curr_readings = {
+			gas: enter_readings(gas),
+			water: enter_readings(water),
+			el_ty: enter_readings(el_ty)
+		}
+		print '\n'
+		# calculate amount
+		amount = {
+			gas: curr_readings[gas] - prev_readings[gas],
+			water: curr_readings[water] - prev_readings[water],
+			el_ty: curr_readings[el_ty] - prev_readings[el_ty]
+		}
+		# print info
+		print_amount()
+		calculate()
+		# save current readings?
+		if 'y' == raw_input('wanna save current record? "y/n" '):
+			readings.append(curr_readings)
+			with open('readings.json', 'w') as f:
+				json.dump(readings, f)
+			print 'record saved'
+	# show tariff
+	elif choise == 's':
+		print_tariff()
+	# change tariff
+	elif choise == 't':
+		tariff = {
+			gas: enter_readings(gas),
+			water: enter_readings(water),
+			el_ty: enter_readings('el_tariff')
+		}
+		with open('tariff.json', 'w') as f:
+			json.dump(tariff, f)
+		print 'tariff changed'
+	# end
+	elif choise == 'q':
+		break
 
 print '\n' + '-' * 8
 print 'good bye!'
