8fb207ff0654d3b2efe064bab836dab64259c6b6
siroghik <siroghik@gmail.com>
2016-05-23 16:08:53 +0300

    python 3.5 support

--- commpays.py
+++ commpays.py
@@ -14,38 +14,38 @@ def enter_readings(r):
 			v = float(input(r + ': '))
 			return v
 		except:
-			print 'invalid format!'
+			print ('invalid format!')
 
 def print_tariff():
-	print '-' * 8
-	print '{0}: {1} for {2}'.format(gas, tariff[gas], 'm3')
-	print '{0}: {1} for {2}'.format(water, tariff[water], 'm3')
-	print '{0} under 100 kWh: {1} for {2}'.format(el_ty, tariff[el_ty][0], 'kWh')
-	print '{0} over 100 kWh: {1} for {2}'.format(el_ty, tariff[el_ty][1], 'kWh')
-	print '-' * 8 + '\n'
+	print ('-' * 8)
+	print ('{0}: {1} for {2}'.format(gas, tariff[gas], 'm3'))
+	print ('{0}: {1} for {2}'.format(water, tariff[water], 'm3'))
+	print ('{0} under 100 kWh: {1} for {2}'.format(el_ty, tariff[el_ty][0], 'kWh'))
+	print ('{0} over 100 kWh: {1} for {2}'.format(el_ty, tariff[el_ty][1], 'kWh'))
+	print ('-' * 8 + '\n')
 
 def print_readings(r):
-	print '-' * 8
-	print '{0}: {1} {2}'.format(gas, r[gas], 'm3')
-	print '{0}: {1} {2}'.format(water, r[water], 'm3')
-	print '{0}: {1} {2}'.format(el_ty, r[el_ty], 'kWh')
-	print '-' * 8 + '\n'
+	print ('-' * 8)
+	print ('{0}: {1} {2}'.format(gas, r[gas], 'm3'))
+	print ('{0}: {1} {2}'.format(water, r[water], 'm3'))
+	print ('{0}: {1} {2}'.format(el_ty, r[el_ty], 'kWh'))
+	print ('-' * 8 + '\n')
 
 def print_amount():
-	print 'amount:\n' + '-' * 8
-	print '{0}: {1} {2}'.format(gas, amount[gas], 'm3')
-	print '{0}: {1} {2}'.format(water, amount[water], 'm3')
-	print '{0}: {1} {2}'.format(el_ty, amount[el_ty], 'kWh')
-	print '-' * 8 + '\n'
+	print ('amount:\n' + '-' * 8)
+	print ('{0}: {1} {2}'.format(gas, amount[gas], 'm3'))
+	print ('{0}: {1} {2}'.format(water, amount[water], 'm3'))
+	print ('{0}: {1} {2}'.format(el_ty, amount[el_ty], 'kWh'))
+	print ('-' * 8 + '\n')
 
 def calculate():
-	print 'pay:\n' + '-' * 8
+	print ('pay:\n' + '-' * 8)
 	pay_for_gas = amount[gas] * tariff[gas]
 	pay_for_gas = round(pay_for_gas, 2)
-	print '{0}: {1} {2}'.format(gas, pay_for_gas, 'UAH')
+	print ('{0}: {1} {2}'.format(gas, pay_for_gas, 'UAH'))
 	pay_for_water = amount[water] * tariff[water]
 	pay_for_water = round(pay_for_water, 2)
-	print '{0}: {1} {2}'.format(water, pay_for_water, 'UAH')
+	print ('{0}: {1} {2}'.format(water, pay_for_water, 'UAH'))
 	pay_for_elty = 0
 	if(amount[el_ty] <= 100):
 		pay_for_elty = amount[el_ty] * tariff[el_ty][0]
@@ -54,13 +54,13 @@ def calculate():
 		over_100 = (amount[el_ty] - 100) * tariff[el_ty][1]
 		pay_for_elty = under_100 + over_100
 	pay_for_elty = round(pay_for_elty, 2)
-	print '{0}: {1} {2}'.format(el_ty, pay_for_elty, 'UAH')
-	print '\nsum: ' + str(pay_for_gas + pay_for_water + pay_for_elty) + 'UAH'
-	print '-' * 8 + '\n'
+	print ('{0}: {1} {2}'.format(el_ty, pay_for_elty, 'UAH'))
+	print ('\nsum: ' + str(pay_for_gas + pay_for_water + pay_for_elty) + 'UAH')
+	print ('-' * 8 + '\n')
 
-print '\n' + '-' * 8
-print 'commpays'
-print '-' * 8
+print ('\n' + '-' * 8)
+print ('commpays')
+print ('-' * 8)
 
 # load tariff
 with open('tariff.json', 'r') as f:
@@ -73,12 +73,12 @@ with open('readings.json', 'r') as f:
 # count old records
 quantity = len(readings)
 if(quantity != 0):
-	print '\nYou have ' + str(quantity) + ' records'
+	print ('\nYou have ' + str(quantity) + ' records')
 	prev_readings = readings[quantity-1]
-	print 'last record:'
+	print ('last record:')
 	print_readings(prev_readings)
 else:
-	print '\nyou have no records'
+	print ('\nyou have no records')
 	prev_readings ={
 		gas: 0.0,
 		water: 0.0,
@@ -94,16 +94,16 @@ t - change tariff
 s - show tariff
 q - quit
 """
-	choise = raw_input(menu_text)
+	choise = input(menu_text)
 	# creat new readings
 	if choise == 'n':
-		print 'please enter current readings'
+		print ('please enter current readings')
 		curr_readings = {
 			gas: enter_readings(gas),
 			water: enter_readings(water),
 			el_ty: enter_readings(el_ty)
 		}
-		print '\n'
+		print ('\n')
 		# calculate amount
 		amount = {
 			gas: curr_readings[gas] - prev_readings[gas],
@@ -114,11 +114,11 @@ q - quit
 		print_amount()
 		calculate()
 		# save current readings?
-		if 'y' == raw_input('wanna save current record? "y/n" '):
+		if 'y' == input('wanna save current record? "y/n" '):
 			readings.append(curr_readings)
 			with open('readings.json', 'w') as f:
 				json.dump(readings, f)
-			print 'record saved'
+			print ('record saved')
 	# show tariff
 	elif choise == 's':
 		print_tariff()
@@ -131,11 +131,11 @@ q - quit
 		}
 		with open('tariff.json', 'w') as f:
 			json.dump(tariff, f)
-		print 'tariff changed'
+		print ('tariff changed')
 	# end
 	elif choise == 'q':
 		break
 
-print '\n' + '-' * 8
-print 'good bye!'
-print '-' * 8 + '\n'
+print ('\n' + '-' * 8)
+print ('good bye!')
+print ('-' * 8 + '\n')
